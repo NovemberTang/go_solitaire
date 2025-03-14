6ed2086ff71e52981865eac373fb0ab916d4a83c
NovemberTang <67543397+NovemberTang@users.noreply.github.com>
2025-03-14 03:52:11 +0000

    implement a card

--- main.go
+++ main.go
@@ -2,6 +2,55 @@ package main
 
 import "fmt"
 
+type Colour string
+
+const (
+	Red   Colour = "red"
+	Black Colour = "black"
+)
+
+type Suit struct {
+	Name   string
+	Colour Colour
+}
+
+var (
+	Hearts   = Suit{Name: "hearts", Colour: Red}
+	Spades   = Suit{Name: "spades", Colour: Black}
+	Diamonds = Suit{Name: "diamonds", Colour: Red}
+	Clubs    = Suit{Name: "clubs", Colour: Black}
+)
+
+type Value int
+
+const (
+	Ace Value = iota
+	One
+	Two
+	Three
+	Four
+	Five
+	Six
+	Seven
+	Eight
+	Nine
+	Ten
+	Jack
+	Queen
+	King
+)
+
+type card struct {
+	Value Value
+	Suit  Suit
+}
+
 func main() {
-	fmt.Println("Hello, World!")
+
+	myCard := card{
+		Value: Three,
+		Suit:  Spades,
+	}
+
+	fmt.Println(myCard)
 }
