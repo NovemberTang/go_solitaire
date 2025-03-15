6747668a42c9c7e721f3e0d94328e4e13ec783e6
NovemberTang <67543397+NovemberTang@users.noreply.github.com>
2025-03-15 00:42:17 +0000

    create a tableau for the final card piles

--- cards/suitstack.go
+++ cards/suitstack.go
@@ -12,28 +12,72 @@ type Stack struct { //todo make generic
 type SuitStack struct {
 	Suit      Suit //could be private?
 	StackHead *Stack
+	size      int
+}
+
+func addToStack(s *SuitStack, card Card) (*SuitStack, error) {
+	s.StackHead = &Stack{card: card, nextCard: s.StackHead}
+	s.size++
+	fmt.Println("Card pushed to stack")
+	return s, nil
 }
 
 func (s *SuitStack) Push(card Card) (*SuitStack, error) {
 	//card must be of the same suit, and one value higher than the current top card
-	if card.Suit != s.Suit {
-		return s, fmt.Errorf("Card suit does not match stack suit")
+	stackIsFull := s.size == 13
+	if stackIsFull {
+		return s, fmt.Errorf("Stack is full")
 	}
 
-	if s.StackHead == nil {
-		if card.Value != Ace {
-			return s, fmt.Errorf("first card in stack must be an Ace")
-		} else {
-			s.StackHead = &Stack{card: card, nextCard: nil}
-			return s, nil
+	cardMatchesSuit := card.Suit == s.Suit
+	if !cardMatchesSuit {
+		return s, fmt.Errorf("cannot add a %s card to a %s stack", card.Suit.Name, s.Suit.Name)
+	}
+
+	nonAceToEmptyStack := s.StackHead == nil && card.Value != Ace
+	if nonAceToEmptyStack {
+		return s, fmt.Errorf("cannot place a %d card on an empty stack", card.Value)
+	}
+
+	invalidNextCardValue := s.StackHead != nil && card.Value-s.StackHead.card.Value != 1
+	if invalidNextCardValue {
+		return s, fmt.Errorf("cannot place a %d value card on top of a %d value card", card.Value, s.StackHead.card.Value)
+	}
+
+	addToStack(s, card)
+	fmt.Println(s.StackHead.card)
+	return s, nil
+}
+
+type Foundation struct {
+	Hearts   SuitStack
+	Spades   SuitStack
+	Diamonds SuitStack
+	Clubs    SuitStack
+}
+
+func (s *Foundation) Push(card Card) (*Foundation, error) {
+	switch card.Suit {
+	case Hearts:
+		_, err := s.Hearts.Push(card)
+		if err != nil {
+			return s, err
+		}
+	case Spades:
+		_, err := s.Spades.Push(card)
+		if err != nil {
+			return s, err
+		}
+	case Diamonds:
+		_, err := s.Diamonds.Push(card)
+		if err != nil {
+			return s, err
+		}
+	case Clubs:
+		_, err := s.Clubs.Push(card)
+		if err != nil {
+			return s, err
 		}
 	}
-
-	if card.Value != s.StackHead.card.Value+1 {
-		return s, fmt.Errorf("current card value is %d. The next card must be %d, not %d", s.StackHead.card.Value, s.StackHead.card.Value+1, card.Value)
-	}
-
-	s.StackHead = &Stack{card: card, nextCard: s.StackHead}
-	fmt.Println("Card pushed to stack")
 	return s, nil
 }
--- main.go
+++ main.go
@@ -9,10 +9,18 @@ func main() {
 
 	// deck := createShuffledDeck()
 	heartStack := &SuitStack{Suit: Hearts, StackHead: nil}
-	heartStack.Push(Card{Value: Ace, Suit: Hearts})
 	_, err := heartStack.Push(Card{Value: Ace, Suit: Hearts})
 	if err != nil {
 		fmt.Println(err)
+	} else {
+		fmt.Println("Card pushed to stack")
+	}
+
+	_, err = heartStack.Push(Card{Value: Ace, Suit: Hearts})
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println("Card pushed to stack")
 	}
 
 }
